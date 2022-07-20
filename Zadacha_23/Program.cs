75c2f7ed289046e52756f8fd73d8f36513d6ac61
Timur <tu-myp@mail.ru>
2022-07-20 19:59:41 +0400

    Задача на таблицу кубов от 1 до N

---
+++ Zadacha_23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+Console.WriteLine("Введите целое число");
+int number = int.Parse(Console.ReadLine());
+
+double Result(int iLocal)
+{
+    return Math.Pow(iLocal, 3);
+}
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write(Result(i) + " ");
+}
