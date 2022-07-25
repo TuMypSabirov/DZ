0b2667230dbb6337368680901b9f4a501577d625
Timur <tu-myp@mail.ru>
2022-07-25 20:37:42 +0400

    Задача на вывод суммы цифр числа

---
+++ Zadacha_27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int Method(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+int sum = Method(num);
+Console.WriteLine(sum);
