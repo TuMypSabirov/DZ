15768b32c6eef30c4d59dcf7df3293e6bbce7bb4
Timur <tu-myp@mail.ru>
2022-08-11 19:24:56 +0400

    Задача на ввод М чисел и подсчет сколько из них больше 0

---
+++ Zadacha_41/Program.cs
@@ -0,0 +1,7 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите числа(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = arr.Count(x => x > 0);
+Console.WriteLine($"Количество положительных чисел: {count}");
