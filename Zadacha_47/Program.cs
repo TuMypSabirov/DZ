e1f63324af83e08e924a2689b383dd2655aa0258
Timur <tu-myp@mail.ru>
2022-08-14 16:12:17 +0400

    Задача на двумерный массив mxn, заполненный случайными числами

---
+++ Zadacha_47/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте двумерный массив размером m×n, заполненный 
+// случайными вещественными числами.
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($" {matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+Console.WriteLine("Введите число строк(m): ");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число столбцов(n): ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[m, n];
+
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
