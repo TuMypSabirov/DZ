f6d777ebffeed6bf544166fbb2295530045955a6
Timur <tu-myp@mail.ru>
2022-08-15 20:24:50 +0400

    Задача про среднее арифметическое элементов в каждом столбце двумерного массива

---
+++ Zadacha_52/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($" {matr[i, j]}    ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void ArithmeticMean(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(1); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < matr.GetLength(0); j++)
+        {
+            sum += matr[j, i];
+        }
+        Console.Write($"{sum / matr.GetLength(0)}" + " | ");
+    }
+}
+
+int[,] matrix = new int[4, 4];
+
+FillArray(matrix);
+Console.WriteLine();
+
+PrintArray(matrix);
+Console.WriteLine();
+
+ArithmeticMean(matrix);
