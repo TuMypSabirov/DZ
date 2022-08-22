ec38cbd2d0ff80335344b95f54797e62b70e99dc
Timur <tu-myp@mail.ru>
2022-08-22 15:52:43 +0400

    Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.

---
+++ Zadacha_64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+
+Console.WriteLine("Введите число M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.WriteLine("Введите число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+string Numbers(int m, int n)
+{
+    if (m == n) return n.ToString();
+    else return m.ToString() + ", " + Numbers(m += 1, n);
+}
+
+Console.WriteLine(Numbers(numM, numN));
