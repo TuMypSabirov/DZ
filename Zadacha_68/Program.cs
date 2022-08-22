19e4456c6c87724a84a73dd6a5e62b4e3bbaf7f0
Timur <tu-myp@mail.ru>
2022-08-22 18:01:29 +0400

    Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.

---
+++ Zadacha_68/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу вычисления функции Аккермана
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+static int FunAckermann(int a, int b)
+{
+    if (a == 0)
+    {
+        return b + 1;
+    }
+    else if (b == 0)
+    {
+        return FunAckermann(a - 1, 1);
+    }
+    else
+    {
+        return FunAckermann(a - 1, FunAckermann(a, b - 1));
+    }
+}
+
+Console.WriteLine("A(m,n)=" + FunAckermann(m, n));
